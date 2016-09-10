022fa52f4e76ba386e72f3ba022984ac6ea7d396
Alain Chautard <achautard@gmail.com>
2016-09-09 17:54:45 -0700

    New example for ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,6 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
 import { HelloComponent } from './hello';
-import { BindingsComponent } from './bindings';
-import { ModelComponent } from './model';
-import { NgforComponent } from './ngfor';
-import { HttpComponent } from './http';
-import { NgifComponent } from './ngif';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import * as comm from "./communication/";
--- src/system-config.ts
+++ src/system-config.ts
@@ -41,6 +41,7 @@ const barrels: string[] = [
   'app/http',
   'app/ngif',
   'app/communication',
+  'app/hiding',
   /** @cli-barrel */
 ];
 

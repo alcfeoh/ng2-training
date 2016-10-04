6ee9a45efca11acdb9666a06babb756e4f6d3e0b
alcfeoh <alain@interstate21.com>
2016-10-04 15:28:03 -0700

    Solution of ngFor lab

---
+++ src/app/ngfor/solution/ngfor-with-index.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngfor',
+  template: ` 
+      <ul>
+        <li *ngFor="let person of persons; let i = index">
+          {{ i + " - " +getFullName(person) }}
+        </li>
+      </ul>`
+})
+export class NgforComponent {
+
+  public persons = [
+    { "firstName": "Bradley", "lastName": "Farmer" },
+    { "firstName": "Cathryn", "lastName": "Maldonado"},
+    { "firstName": "John","lastName": "Cooper" },
+    { "firstName": "Reid", "lastName": "Campos" },
+    { "firstName": "Iva", "lastName": "Briggs" },
+    { "firstName": "Vera", "lastName": "Byers" }
+  ];
+
+  getFullName(person) {
+    return person.firstName + " " + person.lastName;
+  }
+
+}

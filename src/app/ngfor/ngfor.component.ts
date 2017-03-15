8da52aa0715b514ab0753ada2dbb7f766a5e8b11
Alain Chautard <achautard@gmail.com>
2017-03-15 12:07:59 -0700

    New list of persons with birthDates, company, etc.

--- src/app/ngfor/ngfor.component.ts
+++ src/app/ngfor/ngfor.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: ` 
       <ul>
         <li *ngFor="let person of persons">
-          {{ getDisplayName(person)  }} - {{person.birthDate | date: "MM/dd/yyyy"}}
+          {{ getDisplayName(person)  }}
         </li>
       </ul>`
 })
@@ -19,7 +19,7 @@ export class NgforComponent {
       "email": "editheaton@dadabase.com",
       "phone": "+1 (854) 486-2088",
       "address": "280 Florence Avenue, Salix, Alaska, 3036",
-      "birthDate": "2015-04-06T01:15:54 +07:00"
+      "birthDate": new Date(1982, 4, 6)
     },
     {
       "_id": "58c8684e854286bcc54c362f",
@@ -28,7 +28,7 @@ export class NgforComponent {
       "email": "tamicurry@visualix.com",
       "phone": "+1 (926) 441-3832",
       "address": "125 Wilson Street, Eden, Idaho, 5316",
-      "birthDate": "2015-10-23T07:25:48 +07:00"
+      "birthDate": new Date(1972, 7, 9)
     },
     {
       "_id": "58c8684e981ec7408af79acc",
@@ -37,7 +37,7 @@ export class NgforComponent {
       "email": "delorisshaffer@gology.com",
       "phone": "+1 (811) 597-2680",
       "address": "384 Joval Court, Whipholt, Federated States Of Micronesia, 6462",
-      "birthDate": "2015-02-11T04:47:20 +08:00"
+      "birthDate": new Date(1982, 5, 21)
     },
     {
       "_id": "58c8684ed5a401d6731ea194",
@@ -46,7 +46,7 @@ export class NgforComponent {
       "email": "everettelliott@xumonk.com",
       "phone": "+1 (934) 506-3386",
       "address": "771 Hamilton Walk, Abiquiu, Maryland, 5911",
-      "birthDate": "2016-02-17T03:35:21 +08:00"
+      "birthDate": new Date(1980, 4, 6)
     },
     {
       "_id": "58c8684ead1a8fc20b4a6afa",
@@ -55,7 +55,7 @@ export class NgforComponent {
       "email": "nevawoods@cowtown.com",
       "phone": "+1 (822) 470-2733",
       "address": "678 Wyona Street, Bendon, South Dakota, 2553",
-      "birthDate": "2017-03-01T05:15:04 +08:00"
+      "birthDate": new Date(1982, 10, 19)
     },
     {
       "_id": "58c8684eaa617b3c3a3ccf65",
@@ -64,12 +64,12 @@ export class NgforComponent {
       "email": "knowleshartman@geekfarm.com",
       "phone": "+1 (997) 471-2723",
       "address": "255 Hinsdale Street, Vaughn, Guam, 8794",
-      "birthDate": "2014-12-13T02:00:23 +08:00"
+      "birthDate": new Date(1965, 1, 4)
     }
   ];
 
   getDisplayName(person) {
-    return person.name + " " + person.company;
+    return person.name + " - Employer: " + person.company;
   }
 
 }

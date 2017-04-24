0bb06b87139402a48149b78b41f33833181d1074
alcfeoh <alain@interstate21.com>
2017-04-24 13:40:28 -0700

    Removed deprecated code from directive example

--- src/app/directive-solution/highlight.directive.ts
+++ src/app/directive-solution/highlight.directive.ts
@@ -6,7 +6,6 @@ import {Directive, ElementRef, HostListener} from '@angular/core';
 export class HighlightDirective {
 
   constructor(private el: ElementRef) {
-
   }
 
   @HostListener('mouseenter') onMouseEnter() {

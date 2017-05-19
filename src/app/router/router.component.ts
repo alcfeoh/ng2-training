31a35440c1fc696bb66f25e635af08ab4c40caaf
alcfeoh <alain@interstate21.com>
2017-05-19 09:19:26 -0700

    Small change to better highlight router outlet

--- src/app/router/router.component.ts
+++ src/app/router/router.component.ts
@@ -5,13 +5,17 @@ import {LoginService} from "./login/login.service";
   selector: 'app-router',
   template: `
 <p *ngIf="! loginService.isUserLoggedIn()">Please log in first</p>
-<router-outlet></router-outlet>
+
       <h1>Component Router</h1>
       <ul>
         <li><a routerLink="/" routerLinkActive="active">Hello World example</a></li>
         <li><a routerLink="/if" routerLinkActive="active">ngIf example</a></li>
         <li><a routerLink="/http" routerLinkActive="active">HTTP example</a></li>
       </ul>
+      
+      
+      <hr>
+      <router-outlet></router-outlet>
       
     `,
 })

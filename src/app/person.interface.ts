095b11285f2669803a8508abf876b375d39ca06a
alcfeoh <alain@interstate21.com>
2017-07-31 16:34:49 -0700

    Added a person interface to allow for strong typing

---
+++ src/app/person.interface.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by AlaiCh on 7/26/2017.
+ */
+export interface Person {
+  _id: string;
+  name: string;
+  company: string;
+  email: string;
+  phone: string;
+  address: string;
+  birthDate: string;
+}

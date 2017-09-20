43026ad426280976b447b57215bf4401c71612a4
Alain Chautard <alain@interstate21.com>
2017-09-20 13:47:38 -0700

    Sample loader component for demo purposes

---
+++ src/app/spinner/spinner.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.css']
+})
+export class SpinnerComponent {
+
+  @Input()
+  isLoading = false;
+
+  constructor() { }
+
+}

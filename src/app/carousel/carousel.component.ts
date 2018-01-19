367df25d591911ec871182b492c20cd9d16c115d
Alain Chautard <achautard@gmail.com>
2018-01-19 15:57:14 -0800

    Solution for lab #7 - ngSwitch

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent {
 
-  constructor() { }
+  slide = 0;
 
-  ngOnInit() {
+  constructor() {
+    setInterval(() => this.nextSlide(), 5000);
   }
 
+  nextSlide() {
+    this.slide = (this.slide + 1) % 3 ;
+  }
+
+  previousSlide() {
+     this.slide = (this.slide === 0 ? 2 : this.slide -1);
+  }
 }

a8d079dcc1c80e44efb9f0d0e717de66419ffa62
Alain Chautard <achautard@gmail.com>
2018-02-05 12:01:15 -0800

    Solution for lab #22

--- src/app/currency-renderer.pipe.spec.ts
+++ src/app/currency-renderer.pipe.spec.ts
@@ -1,8 +1,29 @@
 import { CurrencyRendererPipe } from './currency-renderer.pipe';
+import {CurrencyService} from './currency-switcher/currency.service';
 
-describe('CurrencyRendererPipe', () => {
+fdescribe('CurrencyRendererPipe', () => {
+
+  const service = new CurrencyService();
+
   it('create an instance', () => {
-    const pipe = new CurrencyRendererPipe();
+    const pipe = new CurrencyRendererPipe(service);
     expect(pipe).toBeTruthy();
+  });
+
+  it('should format USD amounts properly', () => {
+    const pipe = new CurrencyRendererPipe(service);
+    expect(pipe.transform(12.558)).toBe('$12.56');
+  });
+
+  it('should format EUR amounts properly', () => {
+    service.setCurrency('EUR');
+    const pipe = new CurrencyRendererPipe(service);
+    expect(pipe.transform(12.558)).toBe('12.56€');
+  });
+
+  it('should change the rate of EUR amounts properly', () => {
+    service.setCurrency('EUR');
+    const pipe = new CurrencyRendererPipe(service);
+    expect(pipe.transform(12.558, 1.5)).toBe('18.84€');
   });
 });

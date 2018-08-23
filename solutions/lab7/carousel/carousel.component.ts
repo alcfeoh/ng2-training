d7c082c7d75cfa52aad7ff0ec49901e3efaf9947
Alain Chautard <achautard@gmail.com>
2018-08-23 10:55:57 -0700

    Solution for lab 7

---
+++ solutions/lab7/carousel/carousel.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent {
+
+  slide = 0;
+
+  constructor() {
+    setInterval(() => this.nextSlide(), 5000);
+  }
+
+  nextSlide() {
+    this.slide = (this.slide + 1) % 3 ;
+  }
+  previousSlide() {
+    this.slide = (this.slide === 0 ? 2 : this.slide -1);
+  }
+
+}

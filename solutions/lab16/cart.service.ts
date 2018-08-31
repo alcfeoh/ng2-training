3321b25f2c17f264a159cec05c1bfca14ea83af8
Alain Chautard <alain@interstate21.com>
2018-08-30 18:50:42 -0700

    Missing file for lab 16 solution

---
+++ solutions/lab16/cart.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {LicensePlate} from './license-plate';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable()
+export class CartService {
+
+  constructor(private http: HttpClient) { }
+
+  getCartContents(): Observable<LicensePlate[]> {
+    return this.http.get<LicensePlate[]>('http://localhost:8000/cart');
+  }
+
+  addToCart(plate: LicensePlate): Observable<any> {
+    return this.http.put<any>('http://localhost:8000/cart/' + plate._id, null);
+  }
+
+  removeFromCart(plate: LicensePlate) {
+    // TODO
+  }
+
+}

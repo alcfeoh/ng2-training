469855427f3668ce5296884dc874f533213c6024
Alain Chautard <achautard@gmail.com>
2018-09-06 15:59:14 -0700

    Solution for lab 22

---
+++ solutions/lab22/currency-renderer.pipe.spec.ts
@@ -0,0 +1,29 @@
+import { CurrencyRendererPipe } from './currency-renderer.pipe';
+import {CurrencyService} from './currency.service';
+
+fdescribe('CurrencyRendererPipe', () => {
+
+    const service = new CurrencyService();
+
+    it('create an instance', () => {
+      const pipe = new CurrencyRendererPipe(service);
+      expect(pipe).toBeTruthy();	    expect(pipe).toBeTruthy();
+    });
+
+    it('should format USD amounts properly', () => {
+      const pipe = new CurrencyRendererPipe(service);
+      expect(pipe.transform(12.558)).toBe('$12.56');
+    });
+
+    it('should format EUR amounts properly', () => {
+      service.setCurrency('EUR');
+      const pipe = new CurrencyRendererPipe(service);
+      expect(pipe.transform(12.558)).toBe('12.56€');
+    });
+
+    it('should change the rate of EUR amounts properly', () => {
+      service.setCurrency('EUR');
+      const pipe = new CurrencyRendererPipe(service);
+      expect(pipe.transform(12.558, 1.5)).toBe('18.84€');
+    });
+});

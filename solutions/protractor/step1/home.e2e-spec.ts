5ff374c40f367cb8f5477d4599b901481a985a54
Alain Chautard <achautard@gmail.com>
2018-11-27 16:55:17 -0800

    Simplified solution for step #1

--- solutions/protractor/step1/home.e2e-spec.ts
+++ solutions/protractor/step1/home.e2e-spec.ts
@@ -3,17 +3,16 @@ import {StoreHomePage} from './home.po';
 describe('License Plate Store home page', () => {
   let page: StoreHomePage;
 
-  beforeEach(async() => {
+  beforeEach(() => {
     page = new StoreHomePage();
-    await page.navigateTo();
+    page.navigateTo();
   });
 
-  it('should have a page title', async() => {
-    let title = await page.getPageTitle();
-    expect(title).toEqual('Angular training');
+  it('should have a page title', () => {
+    expect(page.getPageTitle()).toEqual('Angular training');
   });
 
-  it('should have the proper text in the header', () => {
+  it('should have proper text in the header', () => {
     expect(page.getNavHeaderTitle()).toEqual('License Plate Store');
   });
 });

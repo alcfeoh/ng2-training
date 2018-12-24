4532d07633895b3b7747a0acb81e5d90a1c81ec4
Alain Chautard <achautard@gmail.com>
2018-12-24 15:33:18 -0800

    Using new UrlTree feature for AuthGuard

--- solutions/lab18/router/auth-guard-service.ts
+++ solutions/lab18/router/auth-guard-service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
-import {CanActivate, Router} from '@angular/router';
-import {Observable, of} from 'rxjs';
+import {CanActivate, Router, UrlTree} from '@angular/router';
 import {LoginService} from './login/login.service';
 
 @Injectable()
@@ -9,12 +8,7 @@ export class AuthGuard implements CanActivate {
   constructor(public service: LoginService, public router: Router) {
   }
 
-  canActivate(): Observable<boolean> {
-    const isLoggedIn = this.service.isUserLoggedIn();
-    if (! isLoggedIn) {
-        this.router.navigateByUrl('login');
-    }
-    return of(isLoggedIn);
+  canActivate(): boolean | UrlTree {
+    return this.service.isUserLoggedIn() || this.router.parseUrl('login');
   }
-
 }

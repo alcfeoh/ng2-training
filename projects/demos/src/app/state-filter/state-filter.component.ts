69043dadcd9c472a16ee58d5f2c80b8ec9aafcb8
Alain Chautard <achautard@gmail.com>
2019-02-18 10:11:01 -0800

    Improved state filter example + margins on demo app

--- projects/demos/src/app/state-filter/state-filter.component.ts
+++ projects/demos/src/app/state-filter/state-filter.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {combineLatest, Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {FormControl} from '@angular/forms';
-import {map} from 'rxjs/operators';
+import {map, startWith} from 'rxjs/operators';
 
 interface State {
   abbreviation: string;
@@ -19,11 +19,13 @@ export class StateFilterComponent {
   states$: Observable<State[]>;
   filteredStates$: Observable<State[]>;
   filter: FormControl;
+  filter$: Observable<string>;
 
   constructor(private http: HttpClient) {
     this.states$ = http.get<State[]>('http://localhost:8000/states');
     this.filter = new FormControl('');
-    this.filteredStates$ = combineLatest(this.states$, this.filter.valueChanges).pipe(
+    this.filter$ = this.filter.valueChanges.pipe(startWith(''));
+    this.filteredStates$ = combineLatest(this.states$, this.filter$).pipe(
       map(([states, filterString]) => states.filter(state => state.name.indexOf(filterString) !== -1))
     );
   }

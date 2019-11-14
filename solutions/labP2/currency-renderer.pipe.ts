32586910f4f3f8a15bd8248845e3e9683edde8a4
Alain Chautard <alain@interstate21.com>
2019-11-14 09:22:18 -0800

    Fixed currency pipe solution

--- solutions/labP2/currency-renderer.pipe.ts
+++ solutions/labP2/currency-renderer.pipe.ts
@@ -8,7 +8,7 @@ export class CurrencyRendererPipe implements PipeTransform {
 
   transform(value: any, currency: string = 'USD', changeRate: number = 1.0): any {
       return new CurrencyPipe('en-US')
-        .transform(value * changeRate, currency, 'symbol', '1.0-2');
+        .transform(value / changeRate, currency, 'symbol', '1.0-2');
   }
 
 }

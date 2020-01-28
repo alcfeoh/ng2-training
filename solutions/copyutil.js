e73895bf0f6aaa538b236174d34244077f0b35fa
alcfeoh <alain@interstate21.com>
2020-01-28 15:00:18 -0800

    Removed solutions from that repo

--- solutions/copyutil.js
+++
@@ -1,4 +0,0 @@
-var cp = require('copyfiles');
-
-cp([  'solutions/'+process.argv[2] + '/*', './src/app'], true, () => {});
-cp([  'solutions/'+process.argv[2] + '/**/*', './src/app'], 2, () => console.log('Done'));
--- solutions/labP2/currency-renderer.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CurrencyRendererPipe } from './currency-renderer.pipe';
-
-describe('CurrencyRendererPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CurrencyRendererPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

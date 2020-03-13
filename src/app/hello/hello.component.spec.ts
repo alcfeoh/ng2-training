ce04fe1caab91839bf552ecdcc8cad01ea46ed5b
alcfeoh <alain@interstate21.com>
2020-03-13 15:05:48 -0700

    Added unit test for HelloComponent

---
+++ src/app/hello/hello.component.spec.ts
@@ -0,0 +1,30 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HelloComponent } from './hello.component';
+import {By} from "@angular/platform-browser";
+
+describe('HelloComponent', () => {
+  let component: HelloComponent;
+  let fixture: ComponentFixture<HelloComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HelloComponent ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HelloComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display "Hello John Angular"', () => {
+    expect(fixture.debugElement.query(By.css('h2')).nativeElement.textContent).toBe('Hello John Angular');
+  });
+});

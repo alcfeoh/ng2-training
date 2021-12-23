9a60418e8df527094779a65e5674b68cefbc56be
alcfeoh <alain@interstate21.com>
2021-12-23 10:51:03 -0800

    Fixed unit tests + npm update

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,6 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
@@ -32,7 +31,7 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
+              ? ['progress']
               : ['progress', 'kjhtml'],
     port: 9876,
     colors: true,

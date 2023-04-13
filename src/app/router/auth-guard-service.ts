8197b71d68292bb39a2aea28048d25996a495ebd
alcfeoh <alain@interstate21.com>
2023-04-13 12:24:09 -0700

    Migrated to Angular 16 RC0

--- src/app/router/auth-guard-service.ts
+++ src/app/router/auth-guard-service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
-import {CanActivate} from '@angular/router';
+
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor() {
   }

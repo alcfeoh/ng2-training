cdc77557be0f57a8296176b70add83ba83a7039f
alcfeoh <alain@interstate21.com>
2023-04-17 15:01:53 -0700

    After computed

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -1,14 +1,15 @@
-import {Injectable, Signal, signal} from '@angular/core';
+import {computed, Injectable, Signal, signal} from '@angular/core';
 import {Currency} from './currency-switcher/currency';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
 
-  private currencySignal = signal<Currency>("USD");
-
-  constructor() { }
+  private currencySignal = signal<Currency>('USD');
+  private symbols = {USD: "$", EUR: "€", GBP: "£"};
+  symbol = computed<string>(() => this.symbols[this.currencySignal()]);
 
   get currency(): Signal<Currency> {
     return this.currencySignal.asReadonly();
@@ -17,4 +18,5 @@ export class CurrencyService {
   setCurrency(curr: Currency): void {
     this.currencySignal.set(curr);
   }
+
 }

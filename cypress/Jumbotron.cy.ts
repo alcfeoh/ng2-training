9fd01ad8f7b084bf9a7e0200e6cf07526f5755ce
alcfeoh <alain@interstate21.com>
2024-01-09 10:36:58 -0800

    Default Jumbotron test updated

--- cypress/Jumbotron.cy.ts
+++ cypress/Jumbotron.cy.ts
@@ -3,9 +3,7 @@ import {JumbotronComponent} from '../src/app/jumbotron/jumbotron.component';
 describe('Jumbotron component tests', () => {
 
   it('should display Jumbotron component with both title and description', () => {
-    cy.mount(`<app-jumbotron>
-              </app-jumbotron>
-    `, {declarations: [JumbotronComponent]});
+    cy.mount(JumbotronComponent);
     cy.contains("Title");
   })
 
